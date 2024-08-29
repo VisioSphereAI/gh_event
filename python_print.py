df299371f2c8f02521b14bbbef600074e4d8b769
amish0 <amishkumar562@gmail.com>
2024-08-29 08:10:01 +0530

    added python script to run

--- python_print.py
+++ python_print.py
@@ -3,3 +3,23 @@ print("Hello")
 print("Bye")
 import numpy as np
 print(np.random.rand(3,3))
+
+print("Hi")
+
+class A:
+    def __init__(self):
+        print("A")
+    
+    def __del__(self):
+        print("Destructing A")
+    
+    def __str__(self):
+        return "A"
+    
+    def __repr__(self):
+        return "A"
+
+if __name__ == "__main__":
+    a = A()
+    print(a)
+    del a
